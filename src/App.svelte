37af0560bb5d9ebe05d39d0793e55da4853e80fb
Igor Santos <532299+igorsantos07@users.noreply.github.com>
2022-08-14 16:04:00 -0300

    #1 Add SCSS

--- src/App.svelte
+++ src/App.svelte
@@ -5,21 +5,22 @@
 
 <main>
   <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite Logo"/>
     </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    <a href="https://svelte.dev" target="_blank">
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo"/>
     </a>
   </div>
   <h1>Vite + Svelte</h1>
 
   <div class="card">
-    <Counter />
+    <Counter/>
   </div>
 
   <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app
+    framework powered by Vite!
   </p>
 
   <p class="read-the-docs">
@@ -27,18 +28,21 @@
   </p>
 </main>
 
-<style>
+<style lang="scss">
   .logo {
     height: 6em;
     padding: 1.5em;
     will-change: filter;
+
+    &:hover {
+      filter: drop-shadow(0 0 2em #646cffaa);
+    }
+
+    &.svelte:hover {
+      filter: drop-shadow(0 0 2em #ff3e00aa);
+    }
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
+
   .read-the-docs {
     color: #888;
   }
